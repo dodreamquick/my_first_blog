973b855423b50f0d079bfb8383f1df304c2bfec9
Ubuntu <ubuntu@ip-172-31-39-211.ap-northeast-2.compute.internal>
2020-02-02 03:24:15 +0000

    Database setting

--- my_first_blog/settings/deploy.py
+++ my_first_blog/settings/deploy.py
@@ -13,13 +13,11 @@ WSGI_APPLICATION = 'my_first_blog.wsgi.deploy.application'
 
 DATABASES = {
     "default" : {
-        'ENGINE' : "django.db.backends.postgresql",
-        'NAME' : "postgres",
+        "ENGINE" : "django.db.backends.postgresql",
+        "NAME" : "blog",
         "USER" : config_secret_deploy["django"]["databases"]["user"],
         "PASSWORD" : config_secret_deploy["django"]["databases"]["password"],
         "HOST" : config_secret_deploy["django"]["databases"]["host"],
-        "PORT" : "5432"
-        
-    }
-    
+        "PORT" : "5432"        
+    }    
 }
